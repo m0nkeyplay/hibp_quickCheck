93729a49689d129419e30be13c51ac91744d11df
~es <46006871+m0nkeyplay@users.noreply.github.com>
2020-04-23 15:29:12 -0500

    Check the ReadMe

--- hibp_check.py
+++ hibp_check.py
@@ -2,6 +2,7 @@
 #
 #   author:		https://github.com/m0nkeyplay
 #   August 26, 2019 - original script written
+#   Updated April 23, 2020 - see README for updates
 #   Free to use
 #   Free to modify and make better
 #   ./hibp_check.py -h for help
@@ -15,8 +16,15 @@ import json
 import os
 import time
 import datetime
-from sys import argv
 import signal
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-b", "--breach", action="store_true")
+ap.add_argument("-p", "--paste", action="store_true")
+ap.add_argument("-e", "--email", required=False, help="Search for just one email")
+ap.add_argument("-f", "--file", required=False, help="grab emails from a list of files -f /path/to/file")
+args = vars(ap.parse_args())
 
 #   CTRL+C handler - from https:/gist.github.com/mikerr/6389549
 def handler(signum, frame):
@@ -27,18 +35,18 @@ signal.signal(signal.SIGINT,handler)
 
 #  Banner Info
 def show_banner():
-  print('################################################################')
-  print('###              have i been pwned  Quick Check              ###')
-  print('###                                                          ###')
-  print('###  Usage: hibp_check.py breach|paste -e email|-f textFile  ###')
-  print('###                                                          ###')
-  print('###  Use and modify at will.                                 ###')
-  print('###  Using the HIBP API v3                                   ###')
-  print('###  https://haveibeenpwned.com/API/v3                       ###')
-  print('###                                                          ###')
-  print('###  *Note that using a file will take some time since the   ###')
-  print('###  API we are calling is rate limited.                     ###')
-  print('################################################################')
+  print('####################################################################')
+  print('###              have i been pwned  Quick Check                  ###')
+  print('###                                                              ###')
+  print('###  Usage: hibp_check.py --breach|--paste -e email|-f textFile  ###')
+  print('###                                                              ###')
+  print('###  Use and modify at will.                                     ###')
+  print('###  All data from:                                              ###')
+  print('###  https://haveibeenpwned.com/                                 ###')
+  print('###                                                              ###')
+  print('###  *Note that using a file will take some time since the       ###')
+  print('###  API we are calling is rate limited.                         ###')
+  print('####################################################################')
 
 # Short Help Menu
 def show_help():
@@ -46,46 +54,37 @@ def show_help():
     print('Which check do you want to do?  breach or paste')
     print('Are you searching one or many emails?')
     print('-e  or -f with textFile having one email per line')
-    print('$: Breach+Email: hibp_check_.py breach -e my@email.com')
-    print('$: Breach+List of emails: hibp_check_.py breach -f ./path/to/file')
+    print('$: Breach+Email: hibp_check_.py -b -e my@email.com')
+    print('$: Breach+List of emails: hibp_check_.py -b -f ./path/to/file')
 
-# Arg Check
-# Sanity Needs to be added
-if len(argv) < 3:
-    show_banner()
-    try:
-        argv[1]
-    except IndexError:
-        print('hibp_check.py -h for help')
-        exit()
-    else:
-        show_help()
-        exit()
+
+if args['breach']:
+    hibpCheck = 'breachaccount'
+elif args['paste']:
+    hibpCheck = 'pasteaccount'
 else:
-    chk=argv[1]
-    if chk == 'breach':
-        hibpCheck = 'breachaccount'
-    elif chk == 'paste':
-        hibpCheck = 'pasteaccount'
-    else:
-        show_banner()
-        print('Check your check type.  The API doesn\'t allow us to search for a %s.'%chk)
-        exit()
-    if argv[2] == '-e':
+    show_banner()
+    show_help()
+    exit()
+
+if args['email']:
       chkType = 'email'
-      chkIt = argv[3]
-    elif argv[2] == '-f':
+      chkIt = args['email']
+elif args['file']:
       chkType = 'file'
-      chkIt = argv[3]
-    else:
-      print('Something is missing - check usage details above.')
-      exit()
+      chkIt = args['file']
+else:
+    show_banner()
+    show_help()
+    exit()   
 
 # Hopefully got through the arg checks, let's build this check
 headers = {}
 headers['content-type']= 'application/json'
 headers['api-version']= '3'
 headers['User-Agent']='the-monkey-playground-script'
+#   Place that API key here
+#   https://haveibeenpwned.com/API/Key
 headers['hibp-api-key']=''
 
 # Check Breach
@@ -93,14 +92,16 @@ def check_breach(eml):
     print('Breach Check for: %s'%eml)
     url = 'https://haveibeenpwned.com/api/v3/breachedaccount/'+eml+'?truncateResponse=false'
     r = requests.get(url, headers=headers)
-    data = r.json()
-    if r.status_code == 200:
+    if r.status_code == 404:
+        print("%s not found. This can be good, or an incorrect email." %eml)
+    elif r.status_code == 200:
+        data = r.json()
         for d in data:
             breach = d['Name']
-            title = d['Title']
             domain = d['Domain']
             breachDate = d['BreachDate']
-            print('Breach: %s\nTitle: %s\nDomain: %s\nBreach Date:%s\n\n'%(breach,title,domain,breachDate))
+            sensitive = d['IsSensitive']
+            print('Account: %s\nBreach: %s\nSensitive: %s\nDomain: %s\nBreach Date:%s\n'%(eml,breach,sensitive,domain,breachDate))
     else:
         print('Error: '+str(r.status_code)+' %s'%(data["message"]))
         exit()
@@ -109,41 +110,46 @@ def check_breach(eml):
 def check_paste(eml):
     print('Paste Check for: %s'%eml)
     url = 'https://haveibeenpwned.com/api/v3/pasteaccount/'+eml
+    print(url)
     r = requests.get(url, headers=headers)
-    data = r.json()
-    if r.status_code == 200:
+    if r.status_code == 404:
+        print("%s not found. This is a good sign, no pastes at the moment." %eml)
+    elif r.status_code == 200:
+        data = r.json()
         for d in data:
             source = d['Source']
             id = str(d['Id'])
             pasteDate = d['Date']
             print('Paste Source: %s\nID: %s\nDate: %s\n\n'%(source,id,pasteDate))
+        #print(r.status_code)
     else:
-        print('Error: '+str(r.status_code)+' %s'%(data["message"]))
+        print('Error: '+str(r.status_code))
         exit()
 
 # Get started
-show_banner()
-# Single Checks
-if chkType == 'email':
-    if hibpCheck == 'breachaccount':
-        check_breach(chkIt)
+if __name__ == '__main__':
+    show_banner()
+    # Single Checks
+    if chkType == 'email':
+        if hibpCheck == 'breachaccount':
+            check_breach(chkIt)
+        else:
+            check_paste(chkIt)
+    # File Checks
+    elif chkType == 'file':
+        if not os.path.isfile(chkIt):
+            print('\n\nWe can\'t find/open %s.  Please check that it\'s a valid file.\n\n'%chkIt)
+        else:
+            get_emails = open(chkIt, 'r')
+            for line in get_emails:
+                cleanEmail = line.strip()
+                if hibpCheck == 'breachaccount':
+                    check_breach(cleanEmail)
+                    time.sleep(2)
+                else:
+                    check_paste(cleanEmail)
+                    time.sleep(2)
+            get_emails.close()
+    # Something really interesting happened
     else:
-        check_paste(chkIt)
-# File Checks
-elif chkType == 'file':
-    if not os.path.isfile(chkIt):
-        print('\n\nWe can\'t find/open %s.  Please check that it\'s a valid file.\n\n'%chkIt)
-    else:
-        get_emails = open(chkIt, 'r')
-        for line in get_emails:
-            cleanEmail = line.strip()
-            if hibpCheck == 'breachaccount':
-                check_breach(cleanEmail)
-                time.sleep(2)
-            else:
-                check_paste(cleanEmail)
-                time.sleep(2)
-        get_emails.close()
-# Something really interesting happened
-else:
-    print('We in trouble.  We should not be here.')
+        print('We in trouble.  We should not be here.')
